098a6f5623d1b09a920018aa04fefe5a484de5d8
Dan Barber <github@danbarber.me>
2015-07-21 17:51:09 +0100

    Put everything in one file (ugh).

--- index.js
+++ index.js
@@ -4,15 +4,102 @@ var async = require('async');
 var request = require('request');
 var moment = require('moment');
 
-var empaticaUtils = require('./lib/empatica/utils.js');
-
 var request = request.defaults({ jar: true });
 
 var userForm = { username: process.env.EMPATICA_USERNAME,
                  password: process.env.EMPATICA_PASSWORD };
 
+var empaticaUtils = {
+  getDeviceName: function(deviceId, deviceModel, deviceLabel, hardwareCode) {
+    var deviceName = "no name";
+
+    if (deviceLabel != null) {
+
+      if (deviceModel.indexOf("E4") > -1) {
+        var pad = "00000";
+        var n = parseInt(deviceLabel).toString(16);
+        var result = "A" + (pad + n).slice(-pad.length);
+
+        deviceName = result.toUpperCase() + "-" + deviceModel;
+      } else {
+        deviceName = deviceLabel.substring(5, 10) + "-" + deviceModel;
+      }
+    } else if (deviceId != null) {
+      deviceName = deviceId  + "-" + deviceModel;
+    } else if (hardwareCode != null) {
+      deviceName = "hw: " + hardwareCode;
+    }
+
+    return deviceName;
+  },
+
+  processSessions: function(sessionData, deviceName) {
+    var deviceRegex = new RegExp('^'+deviceName);
+    return sessionData.map(function (session) {
+      session.device_name = this.getDeviceName(session.device_id, session.device, session.label);
+      return session;
+    }.bind(this))
+    .filter(function (session) {
+      return session.device_name.match(deviceRegex);
+    });
+  },
+
+  processEDA: function(data) {
+    var lines = data.split('\n');
+    return lines.map(function (line) {
+      return parseInt(line, 36) / 10000;
+    });
+  },
+
+  processBVP: function(data) {
+    var lines = data.split('\n');
+    return lines.map(function (line) {
+      return -parseInt(line, 36) / 10;
+    });
+  },
+
+  processACC: function(data) {
+    var lines = data.split('\n');
+    return lines.map(function (line) {
+      return parseInt(line, 36)/100;
+    });
+  },
+
+  processIBI: function(data) {
+    var lines = data.split('\n');
+    var startingTimestamp = 0;
+    var processedData = [];
+
+    lines.forEach(function(line, lineNo) {
+      if (lineNo < lines.length - 3){
+        var values = line.split(',');
+        if (lineNo == 0){
+          if (parseFloat(values[0]) < 60*60*24*10) {
+            startingTimestamp=(parseFloat(values[0]) + correctTimestamp);
+            //console.log(startingTimestamp);
+          }
+          else {
+            startingTimestamp = parseFloat(values[0]) * 1000;
+          }
+        }else{
+          processedData.push([startingTimestamp + parseFloat(values[0]) * 1000, 60 / parseFloat(values[1])]);
+        }
+      }
+    });
+
+    return processedData;
+  },
+
+  processTEMP: function(data) {
+    var lines = data.split('\n');
+    return lines.map(function (line) {
+      return parseInt(line,36)/1000;
+    });
+  }
+}
+
 var empaticaConnect = {
-  getLastSession: function (bandId, success, error) {
+  getLastSession: function (bandId, success, failure) {
     request
     .post('https://www.empatica.com/connect/caramba.php', { form: userForm })
     .on('response', function (response) {
@@ -33,27 +120,32 @@ var empaticaConnect = {
           var processedSessions = empaticaUtils.processSessions(JSON.parse(body), bandId);
           var session = processedSessions.slice(-1)[0];
 
-          session.data = {};
-          var dataUrl = 'https://www.empatica.com/connect/get_csv_proxy.php?id='+session.id+'&file=';
-          var dataTypes = ['eda', 'bvp', 'acc', 'ibi', 'temp'];
-          var dataUrls = dataTypes.map(function (type) { return dataUrl + type; });
+          if (session != undefined) {
+            session.data = {};
+            var dataUrl = 'https://www.empatica.com/connect/get_csv_proxy.php?id='+session.id+'&file=';
+            var dataTypes = ['eda', 'bvp', 'acc', 'ibi', 'temp'];
+            var dataUrls = dataTypes.map(function (type) { return dataUrl + type; });
 
-          async.map(dataUrls, request.get, function (error, results) {
-            dataTypes.forEach(function (type, index) {
-              session.data[type] = empaticaUtils['process'+type.toUpperCase()](results[index].body);
+            async.map(dataUrls, request.get, function (error, results) {
+              dataTypes.forEach(function (type, index) {
+                session.data[type] = empaticaUtils['process'+type.toUpperCase()](results[index].body);
+              });
+              success(session.data.ibi);
             });
-            success(session.data.ibi);
-          });
+          }
+          else {
+            failure('No sessions found.');
+          }
 
         });
       })
       .on('error', function (response) {
-        error('There was a problem receiving the sessions.');
+        failure('There was a problem receiving the sessions.');
       });
     })
 
     .on('error', function (response) {
-      error('There was a problem logging in.');
+      failure('There was a problem logging in.');
     });
   }
 };
